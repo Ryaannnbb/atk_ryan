0a882a771d7d58fe17da04287a36067aa6a510f7
Ryaannn <144214893+Ryaannnbb@users.noreply.github.com>
2024-08-20 13:28:28 +0700

    add test

--- resources/views/beranda.blade.php
+++ resources/views/beranda.blade.php
@@ -40,7 +40,7 @@
         <div class="row">
             <div class="col text-center">
                 <h2>Selamat Datang di Aplikasi Toko ATK Ryan</h2>
-                <p>Disini menyediakan berbagai macam alat tulis atau alat sekolah</p>
+                <p>Disini menyediakan berbagai macam alat tulis atau alat sekolah dan lain lain</p>
             </div>
         </div>
     </div>
